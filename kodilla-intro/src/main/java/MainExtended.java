69667d3fd972d6ff92a83ad83c7efaba299a8f47
Bartosz Witaszek <bartoszwit11@gmail.com>
2022-12-07 00:56:11 +0100

    Prework Task 2

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Bartosz";
+        String eyeColor = "Blue";
+        int numberOfToes = 10;
+        double numberPi = 3.14;
+        char firstLetterName = 'B';
+        boolean isBeginnerTester = true;
+        System.out.println(myName);
+        System.out.println(eyeColor);
+        System.out.println(numberOfToes);
+        System.out.println(numberPi);
+        System.out.println(firstLetterName);
+        System.out.println(isBeginnerTester);
+    }
+}
