7c15e59ae8368b8117d0d5f77e8956d006bc1f19
Bartosz Witaszek <bartoszwit11@gmail.com>
2023-01-26 01:31:28 +0100

    Task Module 3.6

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,32 +1,35 @@
 package com.kodilla.basic_assertion;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTestSuite {
+    Calculator calculator;
+    int a,b;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+        a = 5;
+        b = 8;
+    }
 
     @Test
     public void testSum() {
-        Calculator calculator = new Calculator();
-        int a = 5;
-        int b = 8;
         int sumResult = calculator.sum(a, b);
         assertEquals(13, sumResult);
     }
 
     @Test
     public void testSubtract() {
-        Calculator calculator = new Calculator();
-        int a = 5;
-        int b = 8;
         int subtractResult = calculator.subtract(a, b);
         assertEquals(-3, subtractResult);
     }
 
     @Test
     public void testSquaring() {
-        Calculator calculator = new Calculator();
         double a = -3.0;
         double b = 0.0;
         double c = 1.5;
