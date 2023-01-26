519adfdd47d035ba5b32b9806ec522312fb0f416
Bartosz Witaszek <bartoszwit11@gmail.com>
2023-01-26 01:29:00 +0100

    Task Module 3.5

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -6,7 +6,15 @@ public class Calculator {
         return a + b;
     }
 
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
     public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public double subtract(double a, double b) {
         return a - b;
     }
 
