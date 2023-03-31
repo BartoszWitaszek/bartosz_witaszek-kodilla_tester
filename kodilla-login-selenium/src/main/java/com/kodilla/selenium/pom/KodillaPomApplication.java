ac4b72c705c94fe97a454141123aaaed7fa072aa
Bartosz Witaszek <bartoszwit11@gmail.com>
2023-04-01 00:06:35 +0200

    Kodilla Selenium - Page Object Model*

--- kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/KodillaPomApplication.java
+++ kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/KodillaPomApplication.java
@@ -6,11 +6,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class KodillaPomApplication {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://kodilla.com/pl/test/login");
         KodillaLoginPom loginPom = new KodillaLoginPom(driver);
-        loginPom.login("test@kodilla.com", "haslo");
+        loginPom.login("test@kodilla.com", "kodilla123");
         driver.close();
     }
 }
